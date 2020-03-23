6a870b3d916e7c2880acd8ec15ad545c1f95a2f8
taoyouyou <853094838@qq.com>
2020-03-23 16:43:42 +0800

    Site updated: 2020-03-23 16:43:41

--- photo/ins.js
+++ photo/ins.js
@@ -116,8 +116,11 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/skique/backup/master/photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/skique/backup/master/photos/' + data.link[i];
+          var minSrc = 'https://gitee.com/taoyouyou/backup/raw/master/min_photos/'+ data.link[i];
+          var src= 'https://gitee.com/taoyouyou/backup/raw/master/photos/'+ data.link[i];
+          
+          // var minSrc = 'https://raw.githubusercontent.com/skique/backup/master/photos/' + data.link[i];
+          // var src = 'https://raw.githubusercontent.com/skique/backup/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
